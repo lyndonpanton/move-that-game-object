3a5de2269a18d9956d3168fae02127ca5de23900
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-02 22:54:24 +0000

    Changed the sprite size to make it more visible

--- Exercise12/Assets/Scripts/Mover.cs
+++ Exercise12/Assets/Scripts/Mover.cs
@@ -7,6 +7,14 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 newScale = transform.localScale;
+
+        newScale.x *= 5;
+        newScale.y *= 5;
+
+        transform.localScale = newScale;
+
+
         Rigidbody2D rb2d= GetComponent<Rigidbody2D>();
 
         rb2d.AddForce(
